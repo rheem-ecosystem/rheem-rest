6123148357726b39ef39366eb9244656fb1f749c
Yasser Idris <yidris@qf.org.qa>
2017-05-10 14:33:32 +0300

    Added reduce operator template, fixes for latest Rheem

--- src/main/java/org/qcri/rheem/rest/endpoints/LatestRun.java
+++ src/main/java/org/qcri/rheem/rest/endpoints/LatestRun.java
@@ -2,7 +2,7 @@ package org.qcri.rheem.rest.endpoints;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.qcri.rheem.core.api.Configuration;
-import org.qcri.rheem.core.util.Monitor;
+import org.qcri.rheem.core.monitor.Monitor;
 import org.qcri.rheem.core.util.fs.FileSystem;
 import org.qcri.rheem.core.util.fs.FileSystems;
 import org.qcri.rheem.rest.config.Config;
@@ -30,8 +30,8 @@ public class LatestRun {
         //response = "{\"run_id\":\"1\",\"stages\":[{\"sequence_number\":0,\"operators\":[{\"is_start\":1,\"is_terminal\":0,\"name\":\"mySource\",\"java_class\":\"org.qcri.rheem.java.operators.JavaTextFileSource\",\"connects_to\":{\"0\":[{\"flatMapOperator\":0,\"via\":\"StreamChannel\"}]}},{\"is_start\":0,\"is_terminal\":0,\"name\":\"flatMapOperator\",\"java_class\":\"org.qcri.rheem.java.operators.JavaFlatMapOperator\",\"connects_to\":{\"0\":[{\"mapOperator\":0,\"via\":\"StreamChannel\"}]}},{\"is_start\":0,\"is_terminal\":0,\"name\":\"mapOperator\",\"java_class\":\"org.qcri.rheem.java.operators.JavaMapOperator\",\"connects_to\":{\"0\":[{\"reduceOp\":0,\"via\":\"StreamChannel\"}]}},{\"is_start\":0,\"is_terminal\":0,\"name\":\"reduceOp\",\"java_class\":\"org.qcri.rheem.java.operators.JavaReduceByOperator\",\"connects_to\":{\"0\":[{\"sinkoperator1\":0,\"via\":\"CollectionChannel\"}]}},{\"is_start\":0,\"is_terminal\":1,\"name\":\"sinkoperator1\",\"java_class\":\"org.qcri.rheem.java.operators.JavaLocalCallbackSink\",\"connects_to\":{}}],\"platform\":\"Java Streams\"}]}";
         Configuration rheemConf = new Configuration(Config.rheemPropertiesUrl);
 
-        String runsDir = rheemConf.getStringProperty(Monitor.DEFAULT_RUNS_DIR_PROPERTY_KEY,
-                Monitor.DEFAULT_RUNS_DIR);
+        String runsDir = rheemConf.getStringProperty(Monitor.DEFAULT_MONITOR_BASE_URL_PROPERTY_KEY,
+                Monitor.DEFAULT_MONITOR_BASE_URL);
 
         String latest_run_id = "1";
 
